b832e404e8ead0080ca5169f5586b58f49f66a9e
Dylan Sturgeon <dylansturg@gmail.com>
2016-05-29 17:36:12 -0400

    create basic weak event proxy - used to subscribe to events while maintaining a weak reference relationsihp between subscriber and signaler

---
+++ src/XamarinUtilsLib/XamarinUtilsLib.Tests/Events/WeakEventHandlerProxyTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using DylanSturg.XamarinUtilsLib;
+
+namespace DylanSturg.XamarinUtilsLib.Tests
+{
+	[TestFixture]
+	public class WeakEventHandlerProxyTests
+	{
+		[Test]
+		public void WeakEventHandlerProxy_Invokes_EventHandler_With_Null_Target()
+		{
+			var testHandler = new EventHandler((sender, e) => { });
+			Assert.IsNull(testHandler.Target, $"{nameof(EventHandler)} under test should have a null {nameof(EventHandler.Target)}");
+			var testProxy = new WeakEventHandlerProxy<EventArgs>(testHandler);
+			Assert.IsNull(testProxy.Target, $"{nameof(WeakEventHandlerProxy<EventArgs>)} should store null Target");
+			Assert.DoesNotThrow(() =>
+								testProxy.RaiseEvent(new object(), new EventArgs()));
+		}
+
+		[Test]
+		public void WeakEventHandlerProxy_RaiseEvent_Invokes_Given_EventHandler()
+		{
+			var handlerInvoked = false;
+			var testHandler = new EventHandler((sender, e) =>
+			{
+				handlerInvoked = true;
+			});
+
+			var testProxy = new WeakEventHandlerProxy<EventArgs>(testHandler);
+			Assert.AreEqual(testHandler.Target, testProxy.Target);
+			testProxy.RaiseEvent(new object(), new EventArgs());
+
+			Assert.IsTrue(handlerInvoked, $"{nameof(WeakEventHandlerProxy<EventArgs>.RaiseEvent)} should invoke EventHandler from constructor");
+		}
+
+		[Test]
+		public void WeakEventHandlerProxy_RaiseEvent_Invokes_With_Given_Sender_And_EventArgs()
+		{
+			var expectedSender = new object();
+			var expectedArgs = new EventArgs();
+
+			var handlerInvoked = false;
+			var testHandler = new EventHandler((sender, args) =>
+			{
+				handlerInvoked = true;
+				Assert.AreEqual(expectedArgs, args);
+				Assert.AreEqual(expectedSender, sender);
+			});
+
+			var testProxy = new WeakEventHandlerProxy<EventArgs>(testHandler);
+			testProxy.RaiseEvent(expectedSender, expectedArgs);
+
+			Assert.IsTrue(handlerInvoked, $"{nameof(WeakEventHandlerProxy<EventArgs>.RaiseEvent)} should invoke EventHandler with given sender and EventArgs");
+		}
+	}
+}
+
---
+++ src/XamarinUtilsLib/XamarinUtilsLib/Events/WeakEventHandlerProxy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace DylanSturg.XamarinUtilsLib
+{
+	public class WeakEventHandlerProxy<TArgs> where TArgs : EventArgs
+	{
+		public object Target
+		{
+			get
+			{
+				object strongTarget;
+				if (weakTarget.TryGetTarget(out strongTarget))
+				{
+					return strongTarget;
+				}
+				return null;
+			}
+		}
+
+		readonly WeakReference<object> weakTarget;
+		readonly MethodInfo eventAction;
+
+		public WeakEventHandlerProxy(EventHandler eventHandler)
+		{
+			weakTarget = new WeakReference<object>(eventHandler.Target);
+			eventAction = eventHandler.GetMethodInfo();
+		}
+
+		public void RaiseEvent(object sender, TArgs args)
+		{
+			object strongTarget = null;
+			weakTarget.TryGetTarget(out strongTarget);
+
+			var actionParams = eventAction.GetParameters();
+			var actionArgs = new object[actionParams.Length];
+
+			if (actionParams.Length >= 1)
+			{
+				actionArgs[0] = sender;
+			}
+
+			if (actionParams.Length >= 2)
+			{
+				actionArgs[1] = args;
+			}
+
+			eventAction.Invoke(strongTarget, actionArgs);
+		}
+
+	}
+}
+
